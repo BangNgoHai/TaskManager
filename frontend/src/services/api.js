147682f071b314fdb0b5184f915eabcadc73f4c7
Hai Bang Ngo <sonbangmtp@gmail.com>
2026-04-18 21:06:44 +0200

    inital6

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:8000' });
+const API = axios.create({ 
+  baseURL: 'https://taskmanager-backend-tl5t.onrender.com' 
+});
 
 // Function to get the token from localStorage
 const getToken = () => localStorage.getItem('token');
